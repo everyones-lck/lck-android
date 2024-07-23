1a448fa027c57e198383ab5fd105389baa6ff2cc
KKJ <kyujin0911@tukorea.ac.kr>
2024-07-24 02:36:20 +0900

    [ADD#3] BaseActivity EditText 외부 영역 터치 시 키보드 내리는 함수 추가

--- app/src/main/java/umc/everyones/lck/presentation/base/BaseActivity.kt
+++ app/src/main/java/umc/everyones/lck/presentation/base/BaseActivity.kt
@@ -1,6 +1,11 @@
 package umc.everyones.lck.presentation.base
 
+import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -20,5 +25,23 @@ abstract class BaseActivity<V : ViewDataBinding>(@LayoutRes val layoutResource: 
         setContentView(binding.root)
         initObserver()
         initView()
+    }
+
+    // EditText 외부 영역 터치 시 키보드 내리기
+    override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
+        if (event?.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is EditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm: InputMethodManager =
+                        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
     }
 }

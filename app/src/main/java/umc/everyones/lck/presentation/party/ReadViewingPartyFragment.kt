190b5085caceb514f1de278e027a0c8938779e5d
KKJ <kyujin0911@tukorea.ac.kr>
2024-07-25 00:19:42 +0900

    [UI/#11] 뷰잉파티 글확인 화면 구현

---
+++ app/src/main/java/umc/everyones/lck/presentation/party/ReadViewingPartyFragment.kt
@@ -0,0 +1,15 @@
+package umc.everyones.lck.presentation.party
+
+import umc.everyones.lck.R
+import umc.everyones.lck.databinding.FragmentReadViewingPartyBinding
+import umc.everyones.lck.presentation.base.BaseFragment
+
+class ReadViewingPartyFragment : BaseFragment<FragmentReadViewingPartyBinding>(R.layout.fragment_read_viewing_party) {
+    override fun initObserver() {
+
+    }
+
+    override fun initView() {
+
+    }
+}

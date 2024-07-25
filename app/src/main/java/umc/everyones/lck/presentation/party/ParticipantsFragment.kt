82091365888dccd3b57e56626e8ae99d56ccac5f
KKJ <kyujin0911@tukorea.ac.kr>
2024-07-26 02:24:55 +0900

    [UI/#11] 뷰잉파티 참여자 목록 UI 구현

--- app/src/main/java/umc/everyones/lck/presentation/party/ParticipantsFragment.kt
+++ app/src/main/java/umc/everyones/lck/presentation/party/ParticipantsFragment.kt
@@ -1,15 +1,51 @@
 package umc.everyones.lck.presentation.party
 
+import androidx.navigation.fragment.findNavController
 import umc.everyones.lck.R
 import umc.everyones.lck.databinding.FragmentParticipantsBinding
+import umc.everyones.lck.domain.model.party.ParticipantItem
 import umc.everyones.lck.presentation.base.BaseFragment
+import umc.everyones.lck.presentation.party.adapter.ParticipantsRVA
 
 class ParticipantsFragment : BaseFragment<FragmentParticipantsBinding>(R.layout.fragment_participants) {
+    private var _participantsRVA: ParticipantsRVA? = null
+    private val participantsRVA get() = _participantsRVA
+
+    private val navigator by lazy {
+        findNavController()
+    }
     override fun initObserver() {
 
     }
 
     override fun initView() {
+        initParticipantRVAdapter()
+        binding.ivParticipantsBackBtn.setOnClickListener {
+            navigator.navigateUp()
+        }
+    }
 
+    private fun initParticipantRVAdapter(){
+        val list = listOf(
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+            ParticipantItem("", "", ""),
+        )
+        _participantsRVA = ParticipantsRVA()
+        binding.rvParticipantsList.adapter = participantsRVA
+        participantsRVA?.submitList(list)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _participantsRVA = null
     }
 }

dc730d98783c86319c7d27dcd5521814d362be74
seongh06 <seongh06@naver.com>
2024-07-30 02:11:29 +0900

    [UI#12] MY Team 변경 UI 수정

--- app/src/main/java/umc/everyones/lck/presentation/mypage/MyPageMyteamFragment.kt
+++ app/src/main/java/umc/everyones/lck/presentation/mypage/MyPageMyteamFragment.kt
@@ -1,5 +1,6 @@
 package umc.everyones.lck.presentation.mypage
 
+import android.view.View
 import dagger.hilt.android.AndroidEntryPoint
 import umc.everyones.lck.R
 import umc.everyones.lck.databinding.FragmentMypageMyteamBinding
@@ -7,14 +8,33 @@ import umc.everyones.lck.presentation.base.BaseFragment
 
 @AndroidEntryPoint
 class MyPageMyteamFragment : BaseFragment<FragmentMypageMyteamBinding>(R.layout.fragment_mypage_myteam) {
+
+    private var isSecondLayoutVisible = false
+
     override fun initObserver() {
 
     }
 
     override fun initView() {
-
+        // Back 버튼 클릭 이벤트
         binding.ivMypageMyteamBack.setOnClickListener {
             requireActivity().onBackPressed()
         }
+
+        // Arrow 버튼 클릭 이벤트
+        binding.btnMypageMyteamArrow.setOnClickListener {
+            toggleSecondLayoutVisibility()
+        }
+    }
+
+    private fun toggleSecondLayoutVisibility() {
+        if (isSecondLayoutVisible) {
+            binding.layoutMypageMyteamList.visibility = View.GONE
+            binding.btnMypageMyteamArrow.setImageResource(R.drawable.ic_arrow_up)
+        } else {
+            binding.layoutMypageMyteamList.visibility = View.VISIBLE
+            binding.btnMypageMyteamArrow.setImageResource(R.drawable.ic_arrow_down)
+        }
+        isSecondLayoutVisible = !isSecondLayoutVisible
     }
 }

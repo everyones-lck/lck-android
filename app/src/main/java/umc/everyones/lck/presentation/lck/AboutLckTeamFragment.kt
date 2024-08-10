65157436f7e9a5d3b6e0947ed80d3b4f585e883b
hyowon0204 <ally0204@naver.com>
2024-08-10 15:06:52 +0900

    [MOD/#8] fcv 사용 대신 뷰페이저랑 텝 연결로 수정

--- app/src/main/java/umc/everyones/lck/presentation/lck/AboutLckTeamFragment.kt
+++ app/src/main/java/umc/everyones/lck/presentation/lck/AboutLckTeamFragment.kt
@@ -8,19 +8,23 @@ import android.widget.TextView
 import androidx.core.content.res.ResourcesCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.tabs.TabLayout
 import dagger.hilt.android.AndroidEntryPoint
 import umc.everyones.lck.R
 import umc.everyones.lck.databinding.FragmentAboutLckTeamBinding
 import umc.everyones.lck.presentation.base.BaseFragment
+import umc.everyones.lck.presentation.lck.adapter.TeamVPAdapter
 import umc.everyones.lck.presentation.lck.data.PlayerData
 import umc.everyones.lck.presentation.lck.util.OnPlayerItemClickListener
+import com.google.android.material.tabs.TabLayoutMediator
 
 @AndroidEntryPoint
 class AboutLckTeamFragment : BaseFragment<FragmentAboutLckTeamBinding>(R.layout.fragment_about_lck_team),
     OnPlayerItemClickListener {
 
     private val navigator by lazy { findNavController() }
+    private lateinit var pagerAdapter: TeamVPAdapter
 
     override fun initObserver() {
 
@@ -28,27 +32,21 @@ class AboutLckTeamFragment : BaseFragment<FragmentAboutLckTeamBinding>(R.layout.
 
     override fun initView() {
         val tabLayout: TabLayout = binding.tbAboutLckTeam
+        val viewPager: ViewPager2 = binding.vpAboutLckTeam
         val backButton: ImageView = binding.ivAboutLckTeamPre
         val nextButton: ImageView = binding.ivAboutLckTeamNext
 
-        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-            override fun onTabSelected(tab: TabLayout.Tab?) {
-                val navHostFragment = childFragmentManager.findFragmentById(R.id.fcv_about_lck_team) as NavHostFragment
-                val navController = navHostFragment.navController
-                when (tab?.position) {
-                    0 -> navController.navigate(R.id.aboutLckRoasterFragment)
-                    1 -> navController.navigate(R.id.aboutLckCoachesFragment)
-                    2 -> navController.navigate(R.id.aboutLckClRoasterFragment)
-                    else -> throw IllegalArgumentException("Invalid tab position")
-                }
-            }
+        pagerAdapter = TeamVPAdapter(this)
+        viewPager.adapter = pagerAdapter
 
-            override fun onTabUnselected(tab: TabLayout.Tab?) {
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "LCK Roaster"
+                1 -> "Coaches"
+                2 -> "LCK CL Roaster"
+                else -> throw IllegalArgumentException("Invalid tab position")
             }
-
-            override fun onTabReselected(tab: TabLayout.Tab?) {
-            }
-        })
+        }.attach()
 
 
         backButton.setOnClickListener {
@@ -57,23 +55,6 @@ class AboutLckTeamFragment : BaseFragment<FragmentAboutLckTeamBinding>(R.layout.
 
         nextButton.setOnClickListener {
             navigator.navigate(R.id.action_aboutLCKTeamFragment_to_aboutLckTeamHistoryFragment)
-        }
-
-        tabLayout.getTabAt(0)?.select()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        initView()
-
-        val tabLayout: TabLayout = binding.tbAboutLckTeam
-
-        if (savedInstanceState == null) {
-            val navHostFragment = childFragmentManager.findFragmentById(R.id.fcv_about_lck_team) as NavHostFragment
-            navHostFragment.navController.navigate(R.id.aboutLckRoasterFragment)
-
-            tabLayout.getTabAt(0)?.select()
         }
     }
 

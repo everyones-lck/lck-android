64bce68ae682ffe9b1450432eb3dbed063bb1a68
KKJ <kyujin0911@tukorea.ac.kr>
2024-08-10 17:18:14 +0900

    [ADD/#19] 뷰잉파티 채팅 메시지 유효성 검사 추가

--- app/src/main/java/umc/everyones/lck/presentation/party/ViewingPartyChatActivity.kt
+++ app/src/main/java/umc/everyones/lck/presentation/party/ViewingPartyChatActivity.kt
@@ -2,17 +2,21 @@ package umc.everyones.lck.presentation.party
 
 import android.content.Context
 import android.content.Intent
+import androidx.core.widget.addTextChangedListener
 import umc.everyones.lck.R
 import umc.everyones.lck.databinding.ActivityViewingPartyChatBinding
 import umc.everyones.lck.domain.model.party.ChatItem
 import umc.everyones.lck.presentation.base.BaseActivity
 import umc.everyones.lck.presentation.party.adapter.ChatRVA
+import umc.everyones.lck.util.extension.drawableOf
+import umc.everyones.lck.util.extension.showCustomSnackBar
 
 class ViewingPartyChatActivity : BaseActivity<ActivityViewingPartyChatBinding>(R.layout.activity_viewing_party_chat) {
     private val chatRVA by lazy {
         ChatRVA()
     }
     override fun initView() {
+        validateMessageSend()
         binding.rvChat.adapter = chatRVA
         val list = listOf(
             ChatItem("ds", "dsds", 1, 1),
@@ -31,6 +35,29 @@ class ViewingPartyChatActivity : BaseActivity<ActivityViewingPartyChatBinding>(R
             ChatItem("ds", "dsds", 0, 14),
             )
         chatRVA.submitList(list)
+    }
+
+    private fun validateMessageSend() {
+        binding.etChatInput.addTextChangedListener(
+            onTextChanged = { text: CharSequence?, _: Int, _: Int, _: Int ->
+                if (text != null) {
+
+                    // 댓글 작성 여부에 따른 전송 버튼 활성화 제어
+                    if(text.isEmpty()){
+                        binding.ivChatSendBtn.setImageDrawable(drawableOf(R.drawable.ic_send))
+                    } else {
+                        binding.ivChatSendBtn.setImageDrawable(drawableOf(R.drawable.ic_send_enabled))
+                    }
+
+                    if (text.length >= 1000) {
+                        showCustomSnackBar(
+                            binding.ivChatSendBtn,
+                            "채팅은 최대 1,000자까지 입력할 수 있어요"
+                        )
+                    }
+                }
+            }
+        )
     }
 
     override fun initObserver() {

97da4da70be95f6b68f148059c5c1c802aff3da9
hyowon0204 <ally0204@naver.com>
2024-08-30 12:12:13 +0900

    [ADD/#47] 선수 정보 화면 JGL,SPT로 포지션 명 수정

--- app/src/main/java/umc/everyones/lck/presentation/lck/AboutLckTeamHistoryFragment.kt
+++ app/src/main/java/umc/everyones/lck/presentation/lck/AboutLckTeamHistoryFragment.kt
@@ -91,11 +91,14 @@ class AboutLckTeamHistoryFragment : BaseFragment<FragmentAboutLckTeamHistoryBind
     }
 
     private fun updateWinningHistory(seasonNameList: List<String>) {
-        val items = adapter.getItems().toMutableList()
+        // '2024 Summer'를 필터링하여 제외
+        val filteredSeasonNameList = seasonNameList.filterNot { it == "2024 Summer" }
 
+        val items = adapter.getItems().toMutableList()
         val winningHistoryIndex = items.indexOfFirst { it.title == "Winning History" }
+
         if (winningHistoryIndex != -1) {
-            items[winningHistoryIndex] = items[winningHistoryIndex].copy(details = seasonNameList)
+            items[winningHistoryIndex] = items[winningHistoryIndex].copy(details = filteredSeasonNameList)
             adapter.updateItems(items)
         }
     }

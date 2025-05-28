d65f10231df3de900faf070c3c0ff245c1a758b8
Eouls <ckrlcks00@gmail.com>
2025-05-28 19:23:35 +0900

    [MOD/#93] 승부 예측 하드코딩 삭제

--- app/src/main/java/umc/everyones/lck/data/dto/response/match/MatchTodayMatchResponseDto.kt
+++ app/src/main/java/umc/everyones/lck/data/dto/response/match/MatchTodayMatchResponseDto.kt
@@ -6,12 +6,14 @@ data class MatchTodayMatchResponseDto(
     val seasonName: String,
     val matchNumber: Int,
     val team1Id: Int,
+    val team1Name: String,
     val team1Logo: String,
     val team2Id: Int,
+    val team2Name: String,
     val team2Logo: String,
     val myVoteTeamId: Int
 )
 {
     fun toMatchTodayMatchModel() =
-        MatchTodayMatchModel(seasonName, matchNumber, team1Id, team1Name = "Gen.G", team1Logo, team2Id, team2Name = "T1", team2Logo, myVoteTeamId)
+        MatchTodayMatchModel(seasonName, matchNumber, team1Id, team1Name, team1Logo, team2Id, team2Name, team2Logo, myVoteTeamId)
 }

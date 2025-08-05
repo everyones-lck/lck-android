469d99f3dd4e15c98ee28ba9e8b03e21707b61e8
Eouls <ckrlcks00@gmail.com>
2025-08-06 01:19:36 +0900

    [MOD/#107] API 호출 수정

--- app/src/main/java/umc/everyones/lck/data/service/match/TodayMatchService.kt
+++ app/src/main/java/umc/everyones/lck/data/service/match/TodayMatchService.kt
@@ -3,6 +3,7 @@ package umc.everyones.lck.data.service.match
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 import umc.everyones.lck.data.dto.BaseResponse
 import umc.everyones.lck.data.dto.request.match.CommonPogRequestDto
@@ -32,7 +33,7 @@ interface TodayMatchService {
 
     @GET("pog/result/{matchId}")
     suspend fun fetchTodayMatchPog(
-        @Query ("match-id") matchId: Long
+        @Query("match-id") matchId: Long
     ): BaseResponse<CommonTodayMatchPogResponseDto>
 
     @POST("votes/set-pog/making")
